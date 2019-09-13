0cd95feafb28761956ed71b8d867ffb02e519baa
kojeje <jerome.suhard@lapiscine.pro>
2019-09-13 16:01:10 +0200

    finalisation page d'accueil

--- wp-content/themes/fabulle_wp/page-839.php
+++ wp-content/themes/fabulle_wp/page-839.php
@@ -1,10 +1,4 @@
 <?php
-  /**
-   * page: cie
-   */
-
-
-
 //OBLIGATOIRE : Récupère les variables globales de Wordpress
   $context = Timber::get_context();
 
@@ -19,62 +13,39 @@
 // tableau d'arguments pour modifier la requête en base
 // de données, et venir récupérer uniquement les trois
 // derniers articles
-  $args_shows = [
-    'post_type' => 'spectacles',
-    ];
-  $args_oldshows = [
-    'post_type' => 'spectacles',
-    'meta_key' => 'dispo',
-    'meta_value' => '2',
-  ];
-  $args_soonshows = [
-    'post_type' => 'spectacles',
-    'meta_key' => 'dispo',
-    'meta_value' => '3',
-  ];
+
+
   $args_dates = [
     'post_type' => 'dates',
     'order'=>'ASC',
     'meta_key' => 'date_show',
     'orderby' => 'date_show',
-
   ];
-  $args_membres = [
-    'post_type' => 'membres',
-    'meta_key' => 'priorite',
-    'orderby' => 'priorite',
-    'order'=>'ASC',
+  $args_shows =[
+    'post_type' => 'spectacles',
+
+    ];
+
+    $args_places = [
+      'post_type' => 'places',
+
+    ];
 
 
-  ];
-  $args_ref = [
-    'post_type' => 'references',
-    'meta_key' => 'date_ref',
-    'orderby' => 'date_ref',
-    'order' => 'DESC',
-    'posts_per_page' => 5,
-  ];
-  $args_places = [
-    'post_type' => 'places',
 
-  ];
 
-  
 
 // récupère les articles en fonction du tableau d'argument $args_posts
 // en utilisant la méthode de Timber get_posts
 // puis on les enregistre dans l'array $context sous la clé "posts"
   $context['shows'] = Timber::get_posts($args_shows);
   $context['events'] = Timber::get_posts($args_dates);
-  $context['soonshows'] = Timber::get_posts($args_soonshows);
-  $context['oldshows'] = Timber::get_posts($args_oldshows);
-  $context['membres'] = Timber::get_posts($args_membres);
-  $context['references'] = Timber::get_posts($args_ref);
   $context['places'] = Timber::get_posts($args_places);
 
+//  $context['refs'] = Timber::get_posts($args_articles3);
 
 
 
-// appelle la vue twig "template-dates.twig" située dans le dossier views
+  // appelle la vue twig "page-34.twig" située dans le dossier views
 // en lui passant la variable $context qui contient notamment ici les articles
-  Timber::render('page-39.twig', $context);
+  Timber::render('page-839.twig', $context);
